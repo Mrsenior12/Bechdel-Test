80a7d64b5241a326c1622cf7f7b3046e3ad03c3b
Mrsenior12 <61933232+Mrsenior12@users.noreply.github.com>
2021-08-22 15:23:59 +0200

    correlation
    I added plots with possible correlation between gender and bugdet, bechdel score and so on.
    Moreover to get gender i had to use "gender_guesser" library

--- Bechdel_Test.py
+++ Bechdel_Test.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from plotnine.geoms.geom import geom
+from plotnine.scales.scale_xy import scale_y_continuous
 import seaborn as sns
 import numpy as np
 import urllib,json
-from plotnine import ggplot,geom_point,aes,geom_line
+from plotnine import ggplot,geom_point,aes,geom_line,geom_smooth,labs
+import gender_guesser.detector as gender
 
 from seaborn.palettes import color_palette
 #What is a Bechdel Test?
@@ -13,8 +16,9 @@ from seaborn.palettes import color_palette
 #  1. Have the Bechdel scores of movies improved over the years?
 #  2. Do movies with higher IMDB ratings have higher Bechdel scores?
 #  3. Do movies with female directors have higher Bechdel scores?
-#  4. Does the budget of a movie have any impact on its Bechdel score?
-#  5. Do movies with higher Bechdel scores generate a larger revenue?
+#  4. Do movies with female directors have higher IMBD ratings?
+#  5. Does the budget of a movie have any impact on its Bechdel score?
+#  6. Do movies with higher Bechdel scores generate a larger revenue?
 
 df = pd.read_json('http://bechdeltest.com/api/v1/getAllMovies')
 print(df.head())
@@ -70,7 +74,7 @@ dfAfter.rename(columns={'rating':'bechdel score'},inplace=True)
 dfAfter['year'] = pd.to_datetime(dfAfter['year'], format='%Y')
 dfAfter['bechdel score'] = dfAfter['bechdel score'].astype('category',copy=False)
 
-print(ggplot(dfAfter,aes('year',color=dfAfter['bechdel score']))
+print(ggplot(dfAfter,aes('year',color='bechdel score'))
     +geom_point(stat='count',show_legend=True)
     +geom_line(stat='count',show_legend=True))
 # As we can see on this plot, most movies before 1990 didn't pass the Bechdel test.
@@ -90,4 +94,96 @@ imbd = movies[['title','rating']]
 
 dfAfter = pd.merge(dfAfter,imbd,how='left',left_on=['title'],right_on=['title'])
 dfAfter = dfAfter.dropna()
-print(dfAfter.head())
+#print(dfAfter.head())
+
+# After me merge these two data sets i'm going to create new data frame with year, bechdel score and rating
+dfNew = dfAfter.groupby(['year','bechdel score']).agg({'rating':'mean'}).reset_index()
+#print(dfNew.head())
+
+print(ggplot(dfNew,aes(x='year',y='rating',color='bechdel score'))
+    +geom_point()
+    +geom_smooth()
+    +scale_y_continuous(name='imbd rating')
+    +labs(color='bechdel score')
+)
+# It appears that movies which passed Bechdel test has lower IMBD ratings than movies that didn't pass this test and I was a bit surprised.
+# Now I want to visualize the relationship between gender of the director and bechdel score and IMBD ratings
+# To do this i'll have to merge another data set from "movielatest.csv"
+# using gender prediction library i'll try to find gender of a director by his name.
+
+latest = pd.read_csv("movielatest.csv",encoding='latin')
+dfLatest = latest[['name','director']]
+#print(dfLatest.head())
+dfLatest.rename(columns={'name':'title'},inplace=True)
+dfLatest = pd.merge(dfAfter,dfLatest,how='left',left_on=['title'],right_on=['title'])
+dfLatest = dfLatest.dropna()
+#print(dfLatest.head())
+
+# Now I want to try to predict gender of the director using his first name
+gen = gender.Detector()
+genders = []
+for i in dfLatest['director']:
+    d = i.split()[0]
+    if gen.get_gender(d) == 'male':
+        genders.append('male')
+
+    elif gen.get_gender(d) == 'female':
+        genders.append('female')
+    
+    else:
+        genders.append("unknown")
+dfLatest['gender'] = genders
+
+# now we have to pop "unknown" gender from our data frame
+dfLatest = dfLatest[dfLatest['gender'] != 'unknown']
+
+# the last part of this task is to add another column with 1/0 where 0 means 'male' and 1 means 'female'
+gender_list = []
+for i in dfLatest['gender']:
+    if i == 'male':
+        gender_list.append(0)
+    else:
+        gender_list.append(1)
+dfLatest['female'] = gender_list
+#print(dfLatest.head())
+
+# Now I'll create count plot to show number of males and females in data frame
+sns.countplot(x='gender',data=dfLatest)
+plt.show()
+
+# As I thought in our data frame there's huge difference in number of men and women directors.
+# It shouldn't be surprise, because Hollywood is known to employ small amount of women directors
+# Now i want to visualize the gender of the director and the Bechdel Score, to see if movies with male director have lower score.
+
+sns.countplot(x='bechdel score',hue='gender',data=dfLatest)
+plt.show()
+# We can see that movies which were directed by a female director are more likely to pass bechdel test.
+
+# Now I'd like to check if movies directed by female have higher IMBD
+print(ggplot(dfLatest, aes(x='year',y='rating',color='gender'))
+    +geom_smooth()
+    +scale_y_continuous(name='IMBD rating')
+    +labs(color='gender'))
+
+# As we can see movies directed by male director used to have higher IMBD score than movies directed by female till late 1990s.
+# In the years following 2000 movies with higher IMBD were directed by female.
+
+# Now I want to see if budget of a movie is somehow correlated with Bechdel score.
+dfLatest['budget'] = latest['budget']
+print(ggplot(dfLatest,aes(x='year',y='budget',color='bechdel score'))
+    +geom_point()
+    +geom_smooth())
+
+# One thing you'll see imemdiately is huge increase movie's budget over the years. 
+# Looks like movies that passed Bechdel test are more likle to have higher budget than movies that score a zero.
+# However there is no strong correlation between budget and bechdel score.
+# I would like to know if gender has impact on movie's budget
+
+print(ggplot(dfLatest,aes(x='year',y='budget',color='gender'))
+    +geom_point()
+    +geom_smooth())
+# We can see that ther's no correlation between gender and movie's budget
+
+
+
+
